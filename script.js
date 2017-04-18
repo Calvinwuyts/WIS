bb8f20e33a463838339196b1ec9ad00b73f4a4b4
Calvinwuyts <jolanwuyts@gmail.com>
2017-04-18 11:46:51 +0200

    worked on API YAY

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function dietclick() {
     console.log('clicked diet button');
 }
 
-function vegfunction(query){
+function vegetarianfn(query){
     console.log(query);
    $.ajax({
         url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&diet=vegetarian&limitLicense=false&number=10&offset=0&query='+query+'&ranking=1', // The URL to the API. You can get this in the API page of the API you intend to consume
@@ -29,3 +29,70 @@ function vegfunction(query){
         }
     })
 }
+// check url params to be sure, because lacto vegetarian has a space in it.
+function lactovegetarianfn(query){
+    console.log(query);
+    $.ajax({
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&diet=lacto-vegetarian&limitLicense=false&number=10&offset=0&query='+query+'&ranking=1', // The URL to the API. You can get this in the API page of the API you intend to consume
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            var results = (data.results);
+            $('#results').empty();
+            for (var i=0; i < results.length; i++){
+                console.log(results[i]);
+                $('#results').append("<p>"+(results[i].title)+"</p><img src="+results[i].image+">");
+            }
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "VUid4dgsO3mshHymsIVwPEjlLQKAp1yijEvjsnMIDG19iY0H0a"); // Enter here your Mashape key
+        }
+    })
+}
+
+function pescatarianfn(query){
+    console.log(query);
+    $.ajax({
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&diet=pescatarian&limitLicense=false&number=10&offset=0&query='+query+'&ranking=1', // The URL to the API. You can get this in the API page of the API you intend to consume
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            var results = (data.results);
+            $('#results').empty();
+            for (var i=0; i < results.length; i++){
+                console.log(results[i]);
+                $('#results').append("<p>"+(results[i].title)+"</p><img src="+results[i].image+">");
+            }
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "VUid4dgsO3mshHymsIVwPEjlLQKAp1yijEvjsnMIDG19iY0H0a"); // Enter here your Mashape key
+        }
+    })
+}
+
+function pescatarianfn(query){
+    console.log(query);
+    $.ajax({
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&diet=pescatarian&limitLicense=false&number=10&offset=0&query='+query+'&ranking=1', // The URL to the API. You can get this in the API page of the API you intend to consume
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            var results = (data.results);
+            $('#results').empty();
+            for (var i=0; i < results.length; i++){
+                console.log(results[i]);
+                $('#results').append("<p>"+(results[i].title)+"</p><img src="+results[i].image+">");
+            }
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "VUid4dgsO3mshHymsIVwPEjlLQKAp1yijEvjsnMIDG19iY0H0a"); // Enter here your Mashape key
+        }
+    })
+}
+
